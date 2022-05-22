821f033c3729cf5aa28134bd619d5b91bf114eab
Timothy Staub <timstaub41@gmail.com>
2022-05-22 12:51:00 -0400

    Module 9.1

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello Node!');
+const profileDataArgs = process.argv.slice(2);
+
+const printProfileData = profileDataArr => {
+    //this
+    for (let i = 0; i < profileDataArr.length; i++) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log('====================');
+
+    // Is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
